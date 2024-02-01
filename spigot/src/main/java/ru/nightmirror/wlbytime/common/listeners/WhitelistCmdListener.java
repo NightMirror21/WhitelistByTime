4d6f3b0b3059e3260fc380ec69ad0cd368b9cf07
NightMirror <main@nightmirror.ru>
2024-02-01 15:23:39 +0300

    Fixes to the command sender wrapper.

--- spigot/src/main/java/ru/nightmirror/wlbytime/common/listeners/WhitelistCmdListener.java
+++ spigot/src/main/java/ru/nightmirror/wlbytime/common/listeners/WhitelistCmdListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
+import ru.nightmirror.wlbytime.common.command.WrapperCommandSender;
 import ru.nightmirror.wlbytime.interfaces.command.ICommandsExecutor;
 
 import java.util.Arrays;
@@ -34,7 +35,7 @@ public class WhitelistCmdListener implements Listener {
                     .trim()
                     .split(" ");
 
-            executor.execute(sender, strings);
+            executor.execute(new WrapperCommandSender(sender), strings);
         }
     }
 
@@ -51,7 +52,7 @@ public class WhitelistCmdListener implements Listener {
                     .trim()
                     .split(" ");
 
-            executor.execute(sender, strings);
+            executor.execute(new WrapperCommandSender(sender), strings);
         }
     }
 }
