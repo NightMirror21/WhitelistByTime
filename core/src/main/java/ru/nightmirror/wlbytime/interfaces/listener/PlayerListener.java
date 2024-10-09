ce2310001d667fe74c38182bcf1e3fbf2ebb8e6f
NightMirror21 <me@nightmirror.ru>
2024-10-09 15:11:20 +0300

    Remove `PlayerListener` interface

--- core/src/main/java/ru/nightmirror/wlbytime/interfaces/listener/PlayerListener.java
+++
@@ -1,7 +0,0 @@
-package ru.nightmirror.wlbytime.interfaces.listener;
-
-import ru.nightmirror.wlbytime.common.database.misc.PlayerData;
-
-public interface PlayerListener {
-    void playerRemoved(PlayerData player);
-}
