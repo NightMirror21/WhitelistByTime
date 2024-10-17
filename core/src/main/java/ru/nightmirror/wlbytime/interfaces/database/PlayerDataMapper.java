04e1020036780844390fbcec741188efe71d2fe7
NightMirror21 <me@nightmirror.ru>
2024-10-17 11:51:38 +0300

    Remove unused predicates and interfaces

--- core/src/main/java/ru/nightmirror/wlbytime/interfaces/database/PlayerDataMapper.java
+++
@@ -1,8 +0,0 @@
-package ru.nightmirror.wlbytime.interfaces.database;
-
-import ru.nightmirror.wlbytime.misc.Mapper;
-import ru.nightmirror.wlbytime.models.PlayerData;
-import ru.nightmirror.wlbytime.models.PlayerDataTable;
-
-public interface PlayerDataMapper extends Mapper<PlayerDataTable, PlayerData> {
-}
--- core/src/main/java/ru/nightmirror/wlbytime/misc/Mapper.java
+++
@@ -1,7 +0,0 @@
-package ru.nightmirror.wlbytime.misc;
-
-public interface Mapper<T, E> {
-    E toEntity(T table);
-
-    T toTable(E entity);
-}
