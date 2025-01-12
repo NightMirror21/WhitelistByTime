110eef7ae66dce20927bdbb8a8cd0a211d0e63be
NightMirror21 <me@nightmirror.ru>
2025-01-12 23:27:31 +0300

    Add EntryService implementation and tests
    Signed-off-by: NightMirror21 <me@nightmirror.ru>

---
+++ core/src/main/java/ru/nightmirror/wlbytime/impl/service/EntryServiceImpl.java
@@ -0,0 +1,58 @@
+package ru.nightmirror.wlbytime.impl.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
+import ru.nightmirror.wlbytime.entry.Entry;
+import ru.nightmirror.wlbytime.interfaces.dao.EntryDao;
+import ru.nightmirror.wlbytime.interfaces.services.EntryService;
+
+import java.util.Collections;
+import java.util.Set;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class EntryServiceImpl implements EntryService {
+
+    EntryDao entryDao;
+
+    @Override
+    public void remove(Entry entry) {
+        entryDao.remove(entry);
+    }
+
+    @Override
+    public @NotNull Entry create(String nickname) {
+        return entryDao.create(nickname);
+    }
+
+    @Override
+    public @NotNull Entry create(String nickname, long untilMs) {
+        if (untilMs <= System.currentTimeMillis()) {
+            throw new IllegalArgumentException("Until must be in the future");
+        }
+
+        return entryDao.create(nickname, untilMs);
+    }
+
+    @Override
+    public void freeze(Entry entry, long durationMs) {
+        if (durationMs <= 0) {
+            throw new IllegalArgumentException("Duration must be positive");
+        }
+
+        entry.freeze(durationMs);
+    }
+
+    @Override
+    public void unfreeze(Entry entry) {
+        entry.unfreeze();
+    }
+
+    @Override
+    public @UnmodifiableView Set<Entry> getEntries() {
+        return Collections.unmodifiableSet(entryDao.getAll());
+    }
+}
--- core/src/main/java/ru/nightmirror/wlbytime/interfaces/services/EntryService.java
+++ core/src/main/java/ru/nightmirror/wlbytime/interfaces/services/EntryService.java
@@ -1,5 +1,7 @@
 package ru.nightmirror.wlbytime.interfaces.services;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
 import ru.nightmirror.wlbytime.entry.Entry;
 
 import java.util.Set;
@@ -7,13 +9,13 @@ import java.util.Set;
 public interface EntryService {
     void remove(Entry entry);
 
-    Entry create(String nickname);
+    @NotNull Entry create(String nickname);
 
-    Entry create(String nickname, long untilMs);
+    @NotNull Entry create(String nickname, long untilMs);
 
     void freeze(Entry entry, long durationMs);
 
     void unfreeze(Entry entry);
 
-    Set<Entry> getEntries();
+    @UnmodifiableView Set<Entry> getEntries();
 }
