d23eb0efef6e66d2b01dc3a212e18a8a3337b099
NightMirror21 <me@nightmirror.ru>
2025-01-23 02:10:20 +0300

    Add expiration logic to LastJoinMonitor
    Signed-off-by: NightMirror21 <me@nightmirror.ru>

--- core/src/main/java/ru/nightmirror/wlbytime/monitor/monitors/LastJoinMonitor.java
+++ core/src/main/java/ru/nightmirror/wlbytime/monitor/monitors/LastJoinMonitor.java
@@ -6,6 +6,7 @@ import ru.nightmirror.wlbytime.config.configs.SettingsConfig;
 import ru.nightmirror.wlbytime.impl.dao.EntryDaoImpl;
 import ru.nightmirror.wlbytime.monitor.Monitor;
 
+import java.time.Instant;
 import java.util.logging.Logger;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -13,12 +14,24 @@ public class LastJoinMonitor extends Monitor {
 
     private static final Logger LOGGER = Logger.getLogger(LastJoinMonitor.class.getSimpleName());
 
+    long lastJoinThresholdMs;
+
     public LastJoinMonitor(EntryDaoImpl dao, SettingsConfig settings) {
         super(dao, settings, settings.isLastJoinMonitorEnabled(), settings.getLastJoinMonitorIntervalMs());
+        lastJoinThresholdMs = settings.getLastJoinExpirationThresholdSeconds() * 1000L;
     }
 
     @Override
     protected void run() {
-
+        dao.getAll().stream().filter(entry -> {
+            if (entry.getLastJoin() != null) {
+                Instant lastJoinInstant = entry.getLastJoin().getLastJoinTime().toInstant();
+                return lastJoinInstant.isBefore(Instant.now().minusMillis(lastJoinThresholdMs));
+            }
+            return false;
+        }).forEach(entry -> {
+            dao.remove(entry);
+            LOGGER.info(String.format("Entry with id %d (nickname: %s) has expired and was removed from database", entry.getId(), entry.getNickname()));
+        });
     }
 }
