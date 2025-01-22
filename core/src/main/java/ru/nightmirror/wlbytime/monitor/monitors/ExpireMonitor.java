2c497f4d067e7dfd592c0e32d23fae3a6549459f
NightMirror21 <me@nightmirror.ru>
2025-01-23 02:10:58 +0300

    Implements ExpireMonitor
    Signed-off-by: NightMirror21 <me@nightmirror.ru>

--- core/src/main/java/ru/nightmirror/wlbytime/monitor/monitors/ExpireMonitor.java
+++ core/src/main/java/ru/nightmirror/wlbytime/monitor/monitors/ExpireMonitor.java
@@ -3,6 +3,7 @@ package ru.nightmirror.wlbytime.monitor.monitors;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import ru.nightmirror.wlbytime.config.configs.SettingsConfig;
+import ru.nightmirror.wlbytime.entry.Entry;
 import ru.nightmirror.wlbytime.impl.dao.EntryDaoImpl;
 import ru.nightmirror.wlbytime.monitor.Monitor;
 
@@ -19,6 +20,9 @@ public class ExpireMonitor extends Monitor {
 
     @Override
     protected void run() {
-
+        dao.getAll().stream().filter(Entry::isInactive).forEach(entry -> {
+            dao.remove(entry);
+            LOGGER.info(String.format("Entry with id %d (nickname: %s) has expired and was removed from database", entry.getId(), entry.getNickname()));
+        });
     }
 }
