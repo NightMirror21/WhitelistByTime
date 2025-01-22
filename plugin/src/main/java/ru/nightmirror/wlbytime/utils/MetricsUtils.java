bd77c9f870d4e1ed050111b8008ade11a1957510
NightMirror21 <me@nightmirror.ru>
2025-01-23 02:41:10 +0300

    Use constant for bStats plugin ID.
    Signed-off-by: NightMirror21 <me@nightmirror.ru>

--- plugin/src/main/java/ru/nightmirror/wlbytime/utils/MetricsUtils.java
+++ plugin/src/main/java/ru/nightmirror/wlbytime/utils/MetricsUtils.java
@@ -7,9 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 @UtilityClass
 public class MetricsUtils {
+
+    private static final int BSTATS_PLUGIN_ID = 13834;
+
     public static void tryToLoad(JavaPlugin plugin) {
         try {
-            new Metrics(plugin, 13834);
+            new Metrics(plugin, BSTATS_PLUGIN_ID);
         } catch (Exception exception) {
             plugin.getLogger().info("Failed to start collecting metrics. " +
                     "The plugin will continue working, but metrics will not be collected.");
