dbef15d48b8e460bcce954e0cc73c4eb785eb637
NightMirror21 <me@nightmirror.ru>
2025-04-30 13:18:41 +0300

    Add unit test for CommandsLoader
    Signed-off-by: NightMirror21 <me@nightmirror.ru>

---
+++ core/src/test/java/ru/nightmirror/wlbytime/command/CommandsLoaderTest.java
@@ -0,0 +1,66 @@
+package ru.nightmirror.wlbytime.command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.nightmirror.wlbytime.command.commands.*;
+import ru.nightmirror.wlbytime.config.configs.CommandsConfig;
+import ru.nightmirror.wlbytime.config.configs.MessagesConfig;
+import ru.nightmirror.wlbytime.interfaces.command.Command;
+import ru.nightmirror.wlbytime.interfaces.finder.EntryFinder;
+import ru.nightmirror.wlbytime.interfaces.services.EntryService;
+import ru.nightmirror.wlbytime.interfaces.services.EntryTimeService;
+import ru.nightmirror.wlbytime.time.TimeConvertor;
+import ru.nightmirror.wlbytime.time.TimeRandom;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class CommandsLoaderTest {
+
+    private CommandsConfig commandsConfig;
+    private MessagesConfig messages;
+    private EntryFinder finder;
+    private TimeConvertor convertor;
+    private EntryService entryService;
+    private TimeRandom random;
+    private EntryTimeService entryTimeService;
+
+    private CommandsLoader commandsLoader;
+
+    @BeforeEach
+    public void setUp() {
+        commandsConfig = mock(CommandsConfig.class);
+        messages = mock(MessagesConfig.class);
+        finder = mock(EntryFinder.class);
+        convertor = mock(TimeConvertor.class);
+        entryService = mock(EntryService.class);
+        random = mock(TimeRandom.class);
+        entryTimeService = mock(EntryTimeService.class);
+
+        commandsLoader = new CommandsLoader(
+                commandsConfig,
+                messages,
+                finder,
+                convertor,
+                entryService,
+                random,
+                entryTimeService
+        );
+    }
+
+    @Test
+    public void shouldLoadAllCommands() {
+        Set<Command> commands = commandsLoader.load();
+
+        assertThat(commands).hasSize(7);
+        assertThat(commands).hasAtLeastOneElementOfType(AddCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(CheckCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(CheckMeCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(FreezeCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(GetAllCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(RemoveCommand.class);
+        assertThat(commands).hasAtLeastOneElementOfType(TimeCommand.class);
+    }
+}
